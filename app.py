14efa19104bd59a2617a080687a5e90fc5024310
AdityaRoy0804 <roy97278@gmail.com>
2026-05-17 11:52:37 +0530

    created /patient endpoint with Path Params

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 
@@ -19,3 +19,12 @@ def hello():
 def view_patients():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve ",example="P001")): ## e.g "P001"
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    #return {"message": "Patient not found"}
+    raise HTTPException(status_code=404, detail="Patient not found")
